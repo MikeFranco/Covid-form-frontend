1635da99669f73e93f46c2f7d7a36d4733176bbe
MikeFranco <mfranco_98@yahoo.com>
2020-05-02 14:32:09 -0500

    centered first question

--- components/form-cmpnts/Form.vue
+++ components/form-cmpnts/Form.vue
@@ -1,20 +1,48 @@
 <template>
   <section>
-    <div class="welcome-text">
-      <h3>¿Tienes temperatura de más de 38°C?</h3>
-      <v-container fluid grid-list-xl>
-        <v-layout row justify-space-between align-center>
-          <v-spacer></v-spacer>
-          <v-flex xs6>
-            <v-radio-group v-model="temperature">
-              <v-radio label="Si" value="5"></v-radio>
-              <v-radio label="No" value="0"></v-radio>
-            </v-radio-group>
-          </v-flex>
-        </v-layout>
-      </v-container>
-    </div>
+    <v-container grid-list-md text-xs-center>
+      <v-layout row wrap>
+        <v-flex xs5></v-flex>
+        <v-flex xs4>
+          <h3>¿Tienes temperatura de más de 38°C?</h3>
+        </v-flex>
+        <v-flex xs12 align-self-center justify-center>
+          <v-card dark color="primary">
+            <v-card-text>12</v-card-text>
+          </v-card>
+        </v-flex>
+        <v-flex v-for="i in 2" :key="`6${i}`" xs6>
+          <v-card dark color="secondary">
+            <v-card-text class="px-0">6</v-card-text>
+          </v-card>
+        </v-flex>
+        <v-flex v-for="i in 3" :key="`4${i}`" xs4>
+          <v-card dark color="primary">
+            <v-card-text class="px-0">4</v-card-text>
+          </v-card>
+        </v-flex>
+        <v-flex v-for="i in 4" :key="`3${i}`" xs3>
+          <v-card dark color="secondary">
+            <v-card-text class="px-0">3</v-card-text>
+          </v-card>
+        </v-flex>
+        <v-flex v-for="i in 6" :key="`2${i}`" xs2>
+          <v-card dark color="primary">
+            <v-card-text class="px-0">2</v-card-text>
+          </v-card>
+        </v-flex>
+        <v-flex v-for="i in 12" :key="`1${i}`" xs1>
+          <v-card dark color="secondary">
+            <v-card-text class="px-0">1</v-card-text>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
   </section>
+  <!-- <v-radio-group v-model="temperature">
+            <v-radio label="Si" value="5"></v-radio>
+            <v-radio label="No" value="0"></v-radio>
+          </v-radio-group> -->
 </template>
 
 <script>
