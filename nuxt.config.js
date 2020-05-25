5c447b9b0237d57c6804e893333e4ebe1ec13ef2
MikeFranco <mfranco_98@yahoo.com>
2020-05-24 19:18:05 -0500

    changed again base

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ import colors from 'vuetify/es5/util/colors';
 export default {
   mode: 'spa',
   router: {
-    base: '/Covid-form-frontend/dist'
+    base: '/Covid-form-frontend/'
   },
   /*
    ** Headers of the page
